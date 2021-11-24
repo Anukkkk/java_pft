3f722a3f1fffb0edfecef2623c40f1d29addd644
anna_porotikova <a.porotikova@litebox.ru>
2021-11-24 19:22:22 +0400

    удалена директория build

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirst.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirst.java
@@ -3,8 +3,13 @@ package ru.stqa.pft.sandbox;
 public class MyFirst {
 
   public static void main (String[] args) {
+String somebody = "world";
+System.out.println ("Hello," +  somebody + "!");
 
-System.out.println ("Hello world!");  
+double l = 8.0;
+double s = l*l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+
 }
 
 }
