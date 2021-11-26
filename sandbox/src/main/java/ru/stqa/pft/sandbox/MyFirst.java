520dbcd706172b1b1016fa4c221cd0499be0287e
anna_porotikova <a.porotikova@litebox.ru>
2021-11-26 21:11:51 +0400

    добавлен вывод пояснения в консоль

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirst.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirst.java
@@ -5,6 +5,6 @@ public class MyFirst {
     public static void main(String[] args) {
         Point p1 = new Point(4, 6);
         Point p2 = new Point(5, 7);
-        System.out.println(Dist.distance(p1, p2));
+        System.out.println("Расстояние между двумя точками равно " + Dist.distance(p1, p2));
     }
 }
