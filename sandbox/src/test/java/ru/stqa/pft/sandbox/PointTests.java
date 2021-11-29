746f8a18227c37d6982b298a3b3499548c36c038
anna_porotikova <a.porotikova@litebox.ru>
2021-11-29 16:18:42 +0400

    добавлены тесты для Задания 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -10,4 +10,17 @@ public class PointTests {
         Point p2 = new Point(5, 7);
         Assert.assertEquals(Dist.distance(p1,p2), 2.8284271247461903);
     }
+    @Test
+    public void testPoint1(){
+        Point p1 = new Point(-5, 0);
+        Point p2 = new Point(0, -5);
+        Assert.assertEquals(Dist.distance(p1,p2), 7.0710678118654755);
+    }
+
+    @Test
+    public void testPoint2(){
+        Point p1 = new Point(-5, -0.5);
+        Point p2 = new Point(0.9, -5);
+        Assert.assertEquals(Dist.distance(p1,p2), 7.420242583635659);
+    }
 }
