95274e26a105fd180658dd125d1bd0fdb0ae3a70
anna_porotikova <a.porotikova@litebox.ru>
2021-12-06 14:33:37 +0400

    рефакторинг testGroupDeletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
@@ -9,12 +8,8 @@ public class GroupDeletionTests extends TestBase {
   public void testGroupDeletion() throws Exception {
     goToGroupPage();
     selectGroup();
-    wd.findElement(By.name("delete")).click();
-    wd.findElement(By.linkText("group page")).click();
-  }
-
-  private void selectGroup() {
-    wd.findElement(By.name("selected[]")).click();
+    deleteSelectedGroups();
+    returnToGroupPage();
   }
 
 
