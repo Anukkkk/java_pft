c9b94e3f61f2e420533b04dd225b53925fd64725
anna_porotikova <a.porotikova@litebox.ru>
2021-12-16 21:09:49 +0400

    Правки для дз 8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -2,11 +2,14 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactCreationTest extends TestBase {
 
     @Test
     public void testCreatingContact() throws Exception {
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         app.getNavigationHelper().goToHomePage();
         app.getContactHelper().createContact(new ContactData("Ann", "Sergeevna", "Porotikova", "ann", "test1"));
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -7,7 +7,6 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-
     app.getNavigationHelper().goToGroupPage();
     app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
   }
