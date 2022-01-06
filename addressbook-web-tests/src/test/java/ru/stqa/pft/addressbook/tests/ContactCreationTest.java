e7a98241e5d5c68a90295a36f6d2c06af6667983
anna_porotikova <a.porotikova@litebox.ru>
2022-01-06 22:50:11 +0400

    дз12

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,23 +1,43 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-    @Test
-    public void testCreatingContact() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null){
+            String[] split = line.split(";");
+            line = reader.readLine();
+            list.add(new Object[]{new ContactData().withFirstName(split[0]).withSurname(split[1]).withSecondName(split[2])});
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testCreatingContact(ContactData contact) throws Exception {
         app.goTo().homePage();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/126.png");
-        ContactData contact = new ContactData().withFirstName("Ann").withSurname("Porotikova10")
-                .withGroupIndex(10).withPhoto(photo);
+        //File photo = new File("src/test/resources/126.png");
+        //ContactData contact = new ContactData().withFirstName("Ann").withSurname("Porotikova10")
+       //         .withGroupIndex(10).withPhoto(photo);
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
