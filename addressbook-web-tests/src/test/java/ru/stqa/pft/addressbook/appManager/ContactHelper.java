65f3016f4657c448e60318da86e70597e0492496
anna_porotikova <a.porotikova@litebox.ru>
2022-01-19 21:07:59 +0400

    дз 16 испр if (creation)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ContactHelper.java
@@ -1,8 +1,10 @@
 package ru.stqa.pft.addressbook.appManager;
 
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -22,18 +24,16 @@ public class ContactHelper extends HelperBase {
         type(By.name("nickname"), contactData.getNickName());
         //attach(By.name("photo"), contactData.getPhoto());
 
-        if (creation) {
-           // Select select = new Select(wd.findElement(By.name("new_group")));
-          // if (select.getOptions().size() > 0 && select.getOptions().size() > contactData.getGroupIndex()) {
-           //     select.selectByIndex(contactData.getGroupIndex());
-            } else {
-              //  select.selectByIndex(select.getOptions().size() - 1);
+        if (creation){
+            if (contactData.getGroups().size() > 0){
+                Assert.assertTrue(contactData.getGroups().size() == 1);
+                new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroups().iterator().next().getName());
             }
-      //  } else {
-          //  Assert.assertFalse(isElementPresent(By.name("new_group")));
+        }else{
+            Assert.assertFalse(isElementPresent(By.name("new_group")));
         }
-      //  click(By.xpath("//div[@id='content']/form/input[21]"));
-   // }
+    }
+
 
     public void modify(ContactData contact) {
         selectContactById(contact.getId());
