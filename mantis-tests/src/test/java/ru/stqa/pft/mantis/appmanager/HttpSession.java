798f12681c8fb0f4c3bc75cd5a7202a8e2030f91
anna_porotikova <a.porotikova@litebox.ru>
2022-01-22 22:45:11 +0400

    8.4. ленивая инициализация

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -35,10 +35,7 @@ public class HttpSession {
         post.setEntity(new UrlEncodedFormEntity(params));
         CloseableHttpResponse response = httpclient.execute(post);
         String body = geTextFrom(response);
-         return body.contains(String.format("<span class=\"italic\">%s</span>", username));
-        //return body.contains(String.format("<a href=\"mantisbt-2.25.2\"mantisbt-2.25.2/account_page.php>%s</a>",username));
-        //return body.contains(String.format("<span class=\"label hidden-xs label-default arrowed\">%s</span>", username));
-
+        return body.contains(String.format("<span class=\"italic\">%s</span>", username));
     }
 
     private String geTextFrom(CloseableHttpResponse response) throws IOException {
