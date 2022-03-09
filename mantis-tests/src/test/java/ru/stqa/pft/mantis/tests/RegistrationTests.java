b7063f2a0aac543c1b4555b4173fd70003a5707b
Анна <shishkanovaan@gmail.com>
2022-03-09 22:00:05 +0400

    лекция 9.2

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
-   @BeforeMethod
+   //@BeforeMethod
     public void startMailSever() {
         app.mail().start();
     }
@@ -23,11 +23,11 @@ public class RegistrationTests extends TestBase {
         long now = System.currentTimeMillis();
         String user = String.format("user%s",now);
         String password = "password";
-        String email = String.format("user%s@localhost", now);
-        //app.james().createUser(user, password);
+        String email = String.format("user%s@localhost.localdomain", now);
+        app.james().createUser(user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
-        //List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
+       // List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, password);
         assertTrue(app.newSession().login(user, password));
@@ -39,7 +39,7 @@ public class RegistrationTests extends TestBase {
         return regex.getText(mailMessage.text);
     }
 
-   @AfterMethod(alwaysRun = true)
+  // @AfterMethod(alwaysRun = true)
     public void stopMail() {
         app.mail().stop();
     }
